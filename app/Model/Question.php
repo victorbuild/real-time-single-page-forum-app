4636e15f2ba8c741502c00926f3db4e39d73888b
victor <victor@mail.kinghan.com>
2019-02-20 18:00:31 +0800

    CRUD for Question along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 use App\User;
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    // protected $fillable = ['title','slug','body','category_id','user_id'];
+
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -19,5 +28,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::apiResource('question','QuestionController');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
