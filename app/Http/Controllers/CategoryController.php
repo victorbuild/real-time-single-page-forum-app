5ca0b717a6d1449aad1301f4a0432d31672ab1a3
victor <victor@mail.kinghan.com>
2019-02-21 15:19:20 +0800

    CRUD for Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Category;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
 class CategoryController extends Controller
 {
     /**
@@ -14,17 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Category::latest()->get();
     }
 
     /**
@@ -36,6 +26,11 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //
+        $category = new Category;
+        $category->name = $request->name;
+        $category->slug = str_slug($request->name);
+        $category->save();
+        return response('created', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +41,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     /**
@@ -69,7 +64,13 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update(
+            [
+                'name'=>$request->name,
+                'slug'=>str_slug($request->name)
+            ]
+        );
+        return response()->json('updated', 202);
     }
 
     /**
@@ -80,6 +81,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::apiResource('question','QuestionController');
+Route::apiResource('category','CategoryController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
