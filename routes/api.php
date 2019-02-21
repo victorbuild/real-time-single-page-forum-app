cc4103d3605a9065850965ab6bf269e3da4d7c84
victor <victor@mail.kinghan.com>
2019-02-21 17:48:02 +0800

    新增喜歡以及不喜歡功能

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::apiResource('question','QuestionController');
+Route::apiResource('/question','QuestionController');
 
-Route::apiResource('category','CategoryController');
+Route::apiResource('/category','CategoryController');
 
-Route::apiResource('question/{question}/reply','ReplyController');
+Route::apiResource('/question/{question}/reply','ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
